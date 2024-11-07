f206c01c32f9a252ac5ef08a2088eefd6f4fd90f
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-11-07 14:02:32 +0530

    Removed Socket

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
-const socketIo = require("socket.io");
 const path = require("path");
 
 const driverRoutes = require("./routes/driver");
@@ -10,7 +9,6 @@ const loadRoutes = require("./routes/load");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 mongoose.connect("mongodb://localhost:27017/VehicleLoadDB", {
   useNewUrlParser: true,
@@ -22,9 +20,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/drivers", driverRoutes);
 app.use("/api/load", loadRoutes);
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("disconnect", () => console.log("User disconnected"));
-});
 
 server.listen(3000, () => console.log("Server running on port 3000"));
